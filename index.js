f80659e92e0d7ff3d7790cd1d8dd77532842b262
Lova Shih <vivalalova0@gmail.com>
2017-07-27 18:33:21 +0800

    No message

--- index.js
+++ index.js
@@ -1,91 +1,181 @@
 var mysql = require('mysql');
 
-var pool = mysql.createPool({
-  connectionLimit: process.env.CONNECTION_LIMIT || 50,
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_TABLE,
-  multipleStatements: true,
-});
-
-pool.query = (sql, values, callback) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      return callback(err, null)
-    }
-
-    if (callback) {
-      //
-    } else {
-      callback = values
-      values = undefined
-    }
-
-    connection.query(sql, values, (err, result) => {
-      callback(err, result)
-      connection.release()
-    })
-  })
+const options = {
+	connectionLimit: process.env.CONNECTION_LIMIT || 50,
+	host: process.env.SQL_HOST || '127.0.0.1',
+	user: process.env.SQL_USER || 'root',
+	password: process.env.SQL_PASSWORD || '123',
+	database: process.env.SQL_TABLE || 'taxigo_dev',
+	multipleStatements: true,
 }
 
-pool.createConnection = () => {
-  return new Promise((resolve, reject) => {
-    pool.getConnection(function (err, connection) {
-      if (err) { // return reject(err)
-        console.log(err)
-      }
+const readerPool = mysql.createPool(options)
+setPool(readerPool)
+const writerPool = mysql.createPool(options);
+setPool(writerPool)
 
-      connection.q = (sql, values) => {
-        return new Promise((resolve, reject) => {
-          if (values) {
-            connection.query(sql, values, (err, result) => {
-              if (err) {
-                reject(err)
-              } else {
-                resolve(result)
-              }
-            })
-          } else {
-            connection.query(sql, (err, result) => {
-              if (err) {
-                reject(err)
-              } else {
-                resolve(result)
-              }
-            })
-          }
-        })
-      }
+function getConnection(sql) {
+	// console.log(this.reader)
+	// console.log(this.writer)
 
-      connection.startTransaction = () => {
-        return new Promise((resolve, reject) => {
-          connection.beginTransaction((err) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(connection)
-            }
-          })
-        })
-      }
+	if (sql.match(/(SELECT|select)/)) {
+		return this.reader
+	}
 
-      connection.commitChange = () => {
-        return new Promise((resolve, reject) => {
-          connection.commit((err) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(connection)
-            }
-          })
-        })
-      }
-
-      resolve(connection)
-    })
-  })
+	return this.writer
 }
 
+function query(sql, values, cb) {
+	console.log('1')
+	// if (callback) {
+	// 	console.log('2')
+	// } else {
+	// 	console.log('3')
+	// 	cb = values
+	// 	values = undefined
+	// }
+	console.log(this.getConnection(sql))
+	// getConnection(sql).query(sql, cb)
+	// console.log(query.sql)
 
-module.exports = pool
+}
+
+//manager
+let manager = {
+	createConnection: () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let reader = await readerPool.createConnection()
+				setConnection(reader)
+				let writer = await writerPool.createConnection()
+				setConnection(writer)
+
+				resolve({
+					reader: reader,
+					writer: writer,
+					startTransaction: async (err, cb) => {
+						try {
+							await reader.startTransaction()
+							await writer.startTransaction()
+							cb(undefined)
+						} catch (e) {
+							cb(e)
+						}
+					},
+					query: query,
+					commit: () => {
+						reader.commit()
+						writer.commit()
+					},
+					rollback: () => {
+						reader.rollback()
+						writer.rollback()
+					},
+					release: () => {
+						reader.release()
+						writer.release()
+					},
+					getConnection: getConnection
+				})
+			} catch (e) {
+				reject(e)
+			}
+		})
+	}
+}
+module.exports = manager
+
+function setPool(pool) {
+	pool.createConnection = () => {
+		return new Promise((resolve, reject) => {
+			pool.getConnection(function (err, connection) {
+				if (err) { // return reject(err)
+					console.log(err)
+				}
+
+				setConnection(connection)
+				resolve(connection)
+			})
+		})
+	}
+
+	pool.query = (sql, values, callback) => {
+		pool.getConnection((err, connection) => {
+			if (callback) {
+				//
+			} else {
+				callback = values
+				values = undefined
+			}
+
+			if (err) {
+				return callback(err, null)
+			}
+
+			connection.query(sql, values, (err, result) => {
+				callback(err, result)
+				connection.release()
+			})
+		})
+	}
+}
+
+function setConnection(connection) {
+	connection.q = (sql, values) => {
+		return new Promise((resolve, reject) => {
+			if (values) {
+				connection.query(sql, values, (err, result) => {
+					if (err) {
+						reject(err)
+					} else {
+						resolve(result)
+					}
+				})
+			} else {
+				connection.query(sql, (err, result) => {
+					if (err) {
+						reject(err)
+					} else {
+						resolve(result)
+					}
+				})
+			}
+		})
+	}
+
+	connection.startTransaction = () => {
+		return new Promise((resolve, reject) => {
+			connection.beginTransaction((err) => {
+				if (err) {
+					reject(err)
+				} else {
+					resolve(connection)
+				}
+			})
+		})
+	}
+
+	connection.commitChange = () => {
+		return new Promise((resolve, reject) => {
+			connection.commit((err) => {
+				if (err) {
+					reject(err)
+				} else {
+					resolve(connection)
+				}
+			})
+		})
+	}
+}
+
+async function a() {
+	let m = await manager.createConnection()
+	m.query('select * from user_info', (e, r) => {
+		// console.log(r[0])
+
+		m.release()
+	})
+}
+
+a()
+
