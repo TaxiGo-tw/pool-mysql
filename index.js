59b153f1a6ad25b1ec1e43449cadbf044f7619c5
Lova Shih <vivalalova0@gmail.com>
2017-08-01 15:55:49 +0800

    No message

--- index.js
+++ index.js
@@ -28,29 +28,56 @@ class Manager {
 
 	async beginTransaction(cb) {
 		try {
-			await reader.startTransaction()
-			await writer.startTransaction()
+			await this.reader.startTransaction()
+			await this.writer.startTransaction()
 			cb(undefined)
 		} catch (e) {
 			cb(e)
 		}
 	}
 
+	async awaitTransaction() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				await this.reader.startTransaction()
+				await this.writer.startTransaction()
+				resolve()
+			} catch (e) {
+				reject(e)
+			}
+		})
+	}
+
 	query(sql, values, cb) {
-		let connection = this.getConnection(sql)
+		let connection = this.getReaderOrWriter(sql)
 		let q = connection.query(sql, values, cb)
-		console.log('[' + (connection.threadId || 'default') + '] :' + q.sql)
+		console.log(connection.logPrefix + '<SQL> : ' + q.sql)
+	}
+
+
+	q(sql, values) {
+		return new Promise((reslove, reject) => {
+			let connection = this.getReaderOrWriter(sql)
+			let q = connection.query(sql, values, (e, r) => {
+				console.log(connection.logPrefix + '<SQL> : ' + q.sql)
+				if (e) {
+					console.log(connection.logPrefix + '<SQL Error> :' + e.message)
+					process.env.NODE_ENV != 'development' ? console.log(e) : 'nothing'
+					reject(e)
+				} else {
+					reslove(r)
+				}
+			})
+		})
 	}
 
 	commit() {
+		console.log('commit')
+
 		return new Promise((resolve, reject) => {
 			this.reader.commit(() => {
-				console.log('commit 1')
 				this.writer.commit(() => {
-					console.log('commit 2')
-
-					this.reader.release()
-					this.writer.release()
+					this.release()
 					resolve()
 				})
 			})
@@ -59,13 +86,11 @@ class Manager {
 
 	rollback() {
 		return new Promise((resolve, reject) => {
-			this.reader.rollback(() => {
-				console.log('rollback 1')
-				this.writer.rollback(() => {
-					console.log('rollback 2')
-
-					this.reader.release()
-					this.writer.release()
+			let x = this.reader.rollback(() => {
+				let y = this.writer.rollback(() => {
+					console.log('[' + (x._connection.threadId || 'default') + '] <SQL> : ' + x.sql)
+					console.log('[' + (y._connection.threadId || 'default') + '] <SQL> : ' + y.sql)
+					this.release()
 					resolve()
 				})
 			})
@@ -73,13 +98,25 @@ class Manager {
 	}
 
 	release() {
-		console.log('release')
-		this.reader.release()
-		this.writer.release()
+		if (readerPool._freeConnections.indexOf(this.reader) == -1) {
+			this.reader.release()
+			console.log(this.reader.logPrefix + '<SQL> : Release')
+		}
+
+		if (writerPool._freeConnections.indexOf(this.writer) == -1) {
+			this.writer.release()
+			console.log(this.writer.logPrefix + '<SQL> : Release')
+		}
 	}
 
-	getConnection(sql) {
-		return (/(SELECT|select)/).test(sql) ? this.reader : this.writer
+	getReaderOrWriter(sql) {
+		if ((/(SELECT|select)/).test(sql)) {
+			console.log('get Reader')
+			return this.reader
+		} else {
+			console.log('get Writer')
+			return this.writer
+		}
 	}
 }
 
@@ -113,8 +150,9 @@ module.exports = manager
 function setPool(pool) {
 	pool.createConnection = () => {
 		return new Promise((resolve, reject) => {
-			pool.getConnection(function (err, connection) {
+			pool.getConnection((err, connection) => {
 				if (err) {
+					console.log(err)
 					return reject(err)
 				}
 				setConnection(connection)
@@ -128,6 +166,7 @@ function setPool(pool) {
 			if (err) {
 				return callback(err, null)
 			}
+			console.log('pool.query')
 
 			connection.query(sql, values, (err, result) => {
 				callback(err, result)
@@ -173,25 +212,27 @@ function setConnection(connection) {
 			})
 		})
 	}
+
+	connection.logPrefix = '[' + (connection.threadId || 'default') + '] '
 }
 
-let app = require('express')()
-app.use('*', async (req, res, cb) => {
-	let connection = await manager.createConnection()
 
-	connection.beginTransaction((e) => {
-		connection.query('insert into test_users SET ?', { user_id: 343 }, async (e, r) => {
-			if (e) {
-				console.log(e)
-				connection.rollback()
-				return res.status(400).send({ err: e })
-			}
-			// connection.commit()
-			// connection.rollback()
-			// connection.release()
-			res.send()
-		})
-	})
-})
-app.listen(3000)
+// let app = require('express')()
+// app.use('*', async (req, res, cb) => {
+// 	let connection = await manager.createConnection()
 
+// 	connection.beginTransaction((e) => {
+// 		connection.query('insert into test_users SET ?', { user_id: 343 }, async (e, r) => {
+// 			if (e) {
+// 				console.log(e)
+// 				connection.rollback()
+// 				return res.status(400).send({ err: e })
+// 			}
+// 			// connection.commit()
+// 			// connection.rollback()
+// 			connection.release()
+// 			res.send()
+// 		})
+// 	})
+// })
+// app.listen(3000)
