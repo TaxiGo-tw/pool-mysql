82258c4c8bb8fef88eefedadedd8fd46aaa11e91
Lova <vivalalova0@gmail.com>
2018-07-09 15:24:35 +0800

    1.1.50

---
+++ model/Types.js
@@ -0,0 +1,23 @@
+class PK {
+
+}
+class Point {
+  constructor(x = 0, y = 0) {
+    this.x = x
+    this.y = y
+  }
+}
+
+class ENUM {
+  static cases(...cases) {
+    const instance = new this()
+    instance._cases = cases
+    return instance
+  }
+}
+
+module.exports = {
+  PK,
+  Point,
+  ENUM
+}
