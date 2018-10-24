bd26e698140f4c5d19876345f2ddfdc714949ef2
vivalalova <vivalalova0@gmail.com>
2018-10-24 15:31:20 +0800

    format

--- model/Schema.js
+++ model/Schema.js
@@ -2,293 +2,293 @@ const pool = require('./Pool')
 const Types = require('./Types')
 
 module.exports = class Base {
-  constructor(dict) {
-    if (dict) {
-      for (const key in dict) {
-        this[key] = dict[key]
-      }
-    }
+	constructor(dict) {
+		if (dict) {
+			for (const key in dict) {
+				this[key] = dict[key]
+			}
+		}
 
-    this._queryValues = []
-    this._query = ''
+		this._queryValues = []
+		this._query = ''
 
-    this._populadtes = []
-  }
+		this._populadtes = []
+	}
 
-  static async native(outSideConnection, sql, values) {
-    if (!sql) {
-      throw 'sql command needed'
-    }
+	static async native(outSideConnection, sql, values) {
+		if (!sql) {
+			throw 'sql command needed'
+		}
 
-    this._connection = outSideConnection || await pool.createConnection()
+		this._connection = outSideConnection || await pool.createConnection()
 
-    try {
-      return await this._connection.q(sql, values)
-    } catch (error) {
-      throw error
-    } finally {
-      if (!outSideConnection) {
-        this._connection.release()
-      }
-    }
-  }
+		try {
+			return await this._connection.q(sql, values)
+		} catch (error) {
+			throw error
+		} finally {
+			if (!outSideConnection) {
+				this._connection.release()
+			}
+		}
+	}
 
-  static SELECT(columns = null) {
-    const object = new this()
-    return object.SELECT(columns)
-  }
+	static SELECT(columns = null) {
+		const object = new this()
+		return object.SELECT(columns)
+	}
 
-  SELECT(columns = null) {
-    switch (columns) {
-      case null: {
-        const keys = this.columns
-          ? Object.keys(this.columns).filter(column => !(this.columns[column] instanceof Array)).join(', ')
-          : '*'
+	SELECT(columns = null) {
+		switch (columns) {
+			case null: {
+				const keys = this.columns
+					? Object.keys(this.columns).filter(column => !(this.columns[column] instanceof Array)).join(', ')
+					: '*'
 
-        this._query = `SELECT ${keys} `
-        break
-      }
-      case '*':
-        this._query = 'SELECT * '
-        break
-      default:
-        this._query = `SELECT ${columns} `
-    }
+				this._query = `SELECT ${keys} `
+				break
+			}
+			case '*':
+				this._query = 'SELECT * '
+				break
+			default:
+				this._query = `SELECT ${columns} `
+		}
 
-    return this
-  }
+		return this
+	}
 
-  SQL_NO_CACHE() {
-    this._query += ' SQL_NO_CACHE '
-    return this
-  }
+	SQL_NO_CACHE() {
+		this._query += ' SQL_NO_CACHE '
+		return this
+	}
 
-  FROM(table = this.constructor.name) {
-    this._query += ` FROM ${table} `
-    return this
-  }
+	FROM(table = this.constructor.name) {
+		this._query += ` FROM ${table} `
+		return this
+	}
 
-  JOIN(whereCaluse, whereCaluse2) { return addQuery.bind(this)('JOIN', whereCaluse, whereCaluse2, false) }
-  LEFTJOIN(whereCaluse, whereCaluse2) { return addQuery.bind(this)('LEFT JOIN', whereCaluse, whereCaluse2, false) }
-  WHERE(whereCaluse, whereCaluse2) { return addQuery.bind(this)('WHERE', whereCaluse, whereCaluse2) }
-  AND(whereCaluse, whereCaluse2) { return addQuery.bind(this)('AND', whereCaluse, whereCaluse2) }
-  OR(whereCaluse, whereCaluse2) { return addQuery.bind(this)('OR', whereCaluse, whereCaluse2) }
-  HAVING(whereCaluse, whereCaluse2) { return addQuery.bind(this)('HAVING', whereCaluse, whereCaluse2) }
+	JOIN(whereCaluse, whereCaluse2) { return addQuery.bind(this)('JOIN', whereCaluse, whereCaluse2, false) }
+	LEFTJOIN(whereCaluse, whereCaluse2) { return addQuery.bind(this)('LEFT JOIN', whereCaluse, whereCaluse2, false) }
+	WHERE(whereCaluse, whereCaluse2) { return addQuery.bind(this)('WHERE', whereCaluse, whereCaluse2) }
+	AND(whereCaluse, whereCaluse2) { return addQuery.bind(this)('AND', whereCaluse, whereCaluse2) }
+	OR(whereCaluse, whereCaluse2) { return addQuery.bind(this)('OR', whereCaluse, whereCaluse2) }
+	HAVING(whereCaluse, whereCaluse2) { return addQuery.bind(this)('HAVING', whereCaluse, whereCaluse2) }
 
-  ORDER_BY(column, sort = 'ASC') {
-    if (column) {
-      this._query += ` ORDER BY ${column} ${sort}`
-    }
-    return this
-  }
+	ORDER_BY(column, sort = 'ASC') {
+		if (column) {
+			this._query += ` ORDER BY ${column} ${sort}`
+		}
+		return this
+	}
 
-  LIMIT(numbers) {
-    if (numbers) {
-      this._query += ` LIMIT ${numbers} `
-    }
-    return this
-  }
+	LIMIT(numbers) {
+		if (numbers) {
+			this._query += ` LIMIT ${numbers} `
+		}
+		return this
+	}
 
-  POPULATE(...fileds) {
-    this._populadtes = fileds
-    return this
-  }
+	POPULATE(...fileds) {
+		this._populadtes = fileds
+		return this
+	}
 
-  INSERT(ignore = false) {
-    this._query = `INSERT ${ignore ? 'IGNORE' : ''} `
-    return this
-  }
+	INSERT(ignore = false) {
+		this._query = `INSERT ${ignore ? 'IGNORE' : ''} `
+		return this
+	}
 
-  static INSERT(ignore = false) {
-    const object = new this()
-    return object.INSERT(ignore)
-  }
+	static INSERT(ignore = false) {
+		const object = new this()
+		return object.INSERT(ignore)
+	}
 
-  INTO(table = this.constructor.name) {
-    this._query += ` INTO ${table}`
-    return this
-  }
+	INTO(table = this.constructor.name) {
+		this._query += ` INTO ${table}`
+		return this
+	}
 
-  static DELETE() {
-    const object = new this()
-    return object.DELETE()
-  }
+	static DELETE() {
+		const object = new this()
+		return object.DELETE()
+	}
 
-  DELETE() {
-    this._query = 'DELETE '
-    return this
-  }
+	DELETE() {
+		this._query = 'DELETE '
+		return this
+	}
 
-  PRINT() {
-    this._print = true
-    return this
-  }
+	PRINT() {
+		this._print = true
+		return this
+	}
 
-  WRITER() {
-    this._forceWriter = true
-    return this
-  }
+	WRITER() {
+		this._forceWriter = true
+		return this
+	}
 
-  WRITER() {
-    this._forceWriter = true
-    return this
-  }
+	WRITER() {
+		this._forceWriter = true
+		return this
+	}
 
-  EXPLAIN() {
-    this._query = 'EXPLAIN ' + this._query
-    return this
-  }
+	EXPLAIN() {
+		this._query = 'EXPLAIN ' + this._query
+		return this
+	}
 
-  async exec(outSideConnection = null) {
-    this._connection = outSideConnection || await pool.createConnection()
-    try {
-      let results
+	async exec(outSideConnection = null) {
+		this._connection = outSideConnection || await pool.createConnection()
+		try {
+			let results
 
-      this._connection.useWriter = this._forceWriter
-      this._forceWriter = false
+			this._connection.useWriter = this._forceWriter
+			this._forceWriter = false
 
-      if (this._print) {
-        this._print = false
-        results = await this._connection.print.q(this._query, this._queryValues)
-      } else {
-        results = await this._connection.q(this._query, this._queryValues)
-      }
+			if (this._print) {
+				this._print = false
+				results = await this._connection.print.q(this._query, this._queryValues)
+			} else {
+				results = await this._connection.q(this._query, this._queryValues)
+			}
 
-      if (this._connection.isSelect(this._query)) {
-        results = results.map(result => new this.constructor(result))
+			if (this._connection.isSelect(this._query)) {
+				results = results.map(result => new this.constructor(result))
 
-        //populate
-        if (this._populadtes.length && results.length) {
+				//populate
+				if (this._populadtes.length && results.length) {
 
-          for (let i = 0; i < this._populadtes.length; i++) {
-            const column = this._populadtes[i]
-            const populateType = this.columns[column]
+					for (let i = 0; i < this._populadtes.length; i++) {
+						const column = this._populadtes[i]
+						const populateType = this.columns[column]
 
-            if (populateType instanceof Array) {//coupons: [Coupons]
-              const type = populateType[0]
+						if (populateType instanceof Array) {//coupons: [Coupons]
+							const type = populateType[0]
 
-              const tColumn = Object.keys(type.columns).filter(c => type.columns[c].name == this.constructor.name)[0]
+							const tColumn = Object.keys(type.columns).filter(c => type.columns[c].name == this.constructor.name)[0]
 
-              const PKColumn = Object.keys(this.columns).filter(column => this.columns[column] == Base.Types.PK)[0]
-              const ids = results.map(result => result[PKColumn])
-              const populates = await type.SELECT().FROM().WHERE(`${tColumn} in (${ids})`).exec(this._connection)
+							const PKColumn = Object.keys(this.columns).filter(column => this.columns[column] == Base.Types.PK)[0]
+							const ids = results.map(result => result[PKColumn])
+							const populates = await type.SELECT().FROM().WHERE(`${tColumn} in (${ids})`).exec(this._connection)
 
-              results.forEach(result => {
-                result[column] = populates.filter(p => p[tColumn] == result[PKColumn])
-              })
-            } else {// coupon: Coupons
-              let ids
-              if (results instanceof Array) {
-                ids = results.filter(result => result[column]).map(result => result[column])
-                if (!ids.length) {
-                  continue
-                }
-              } else if (results && results[column]) {
-                ids = [results[column]]
-                if (!ids) {
-                  continue
-                }
-              } else {
-                continue
-              }
+							results.forEach(result => {
+								result[column] = populates.filter(p => p[tColumn] == result[PKColumn])
+							})
+						} else {// coupon: Coupons
+							let ids
+							if (results instanceof Array) {
+								ids = results.filter(result => result[column]).map(result => result[column])
+								if (!ids.length) {
+									continue
+								}
+							} else if (results && results[column]) {
+								ids = [results[column]]
+								if (!ids) {
+									continue
+								}
+							} else {
+								continue
+							}
 
-              const PKColumn = Object.keys(populateType.columns).filter(column => populateType.columns[column] == Base.Types.PK)[0]
-              const populates = await populateType.SELECT().FROM().WHERE(`${PKColumn} IN (${ids})`).exec(this._connection)
+							const PKColumn = Object.keys(populateType.columns).filter(column => populateType.columns[column] == Base.Types.PK)[0]
+							const populates = await populateType.SELECT().FROM().WHERE(`${PKColumn} IN (${ids})`).exec(this._connection)
 
-              results = results.map(result => {
-                if (result[column]) {
-                  result[column] = populates.filter(populate => result[column] == populate[PKColumn])[0] || result[column]
-                }
-                return result
-              })
-            }
-          }
-        }
+							results = results.map(result => {
+								if (result[column]) {
+									result[column] = populates.filter(populate => result[column] == populate[PKColumn])[0] || result[column]
+								}
+								return result
+							})
+						}
+					}
+				}
 
-        results.forEach(this.constructor.REMOVE_PRIVATE_VARIABLE)
-      }
+				results.forEach(this.constructor.REMOVE_PRIVATE_VARIABLE)
+			}
 
-      return results
-    } catch (error) {
-      throw error
-    } finally {
-      if (!outSideConnection) {
-        this._connection.release()
-      }
-    }
-  }
+			return results
+		} catch (error) {
+			throw error
+		} finally {
+			if (!outSideConnection) {
+				this._connection.release()
+			}
+		}
+	}
 
-  static REMOVE_PRIVATE_VARIABLE(t) {
-    delete t._populadtes
-    delete t._query
-    delete t._queryValues
-  }
+	static REMOVE_PRIVATE_VARIABLE(t) {
+		delete t._populadtes
+		delete t._query
+		delete t._queryValues
+	}
 
-  get JSON() {
-    return this
-  }
+	get JSON() {
+		return this
+	}
 
-  get PRIVATE() {
-    return this
-  }
+	get PRIVATE() {
+		return this
+	}
 
-  static get columns() {
-    const instance = new this()
-    if (instance.columns) {
-      return instance.columns
-    }
-    return {}
-  }
+	static get columns() {
+		const instance = new this()
+		if (instance.columns) {
+			return instance.columns
+		}
+		return {}
+	}
 
-  static get Types() { return Types }
+	static get Types() { return Types }
 
 
-  //////////////////////////////Base.js
-  static UPDATE() {
-    const object = new this()
-    return object.UPDATE()
-  }
+	//////////////////////////////Base.js
+	static UPDATE() {
+		const object = new this()
+		return object.UPDATE()
+	}
 
-  UPDATE() {
-    this._query = `UPDATE ${this.constructor.name}`
-    return this
-  }
+	UPDATE() {
+		this._query = `UPDATE ${this.constructor.name}`
+		return this
+	}
 
-  SET(value) {
-    this._query += ' SET ? '
-    this._queryValues.push(value)
-    return this
-  }
+	SET(value) {
+		this._query += ' SET ? '
+		this._queryValues.push(value)
+		return this
+	}
 
-  DUPLICATE(set) {
-    this._query += ' ON DUPLICATE KEY UPDATE ? '
-    this._queryValues.push(set)
-    return this
-  }
+	DUPLICATE(set) {
+		this._query += ' ON DUPLICATE KEY UPDATE ? '
+		this._queryValues.push(set)
+		return this
+	}
 }
 
 function addQuery(reservedWord, whereCaluse, whereCaluse2 = 'oooooooo', inBrackets = true) {
-  if (!whereCaluse) {
-    return this
-  }
+	if (!whereCaluse) {
+		return this
+	}
 
-  if (typeof whereCaluse == 'string') {
+	if (typeof whereCaluse == 'string') {
 
-    if (inBrackets) {
-      this._query += ` ${reservedWord} (${whereCaluse}) `
-    } else {
-      this._query += ` ${reservedWord} ${whereCaluse} `
-    }
+		if (inBrackets) {
+			this._query += ` ${reservedWord} (${whereCaluse}) `
+		} else {
+			this._query += ` ${reservedWord} ${whereCaluse} `
+		}
 
-    if (whereCaluse2 != 'oooooooo' && whereCaluse2 instanceof Array) {
-      this._queryValues = this._queryValues.concat(whereCaluse2)
-    } else if (whereCaluse2 != 'oooooooo') {
-      this._queryValues.push(whereCaluse2)
-    }
-  } else {
-    this._queryValues.push(whereCaluse)
-    this._query += ` ${reservedWord} ? `
-  }
+		if (whereCaluse2 != 'oooooooo' && whereCaluse2 instanceof Array) {
+			this._queryValues = this._queryValues.concat(whereCaluse2)
+		} else if (whereCaluse2 != 'oooooooo') {
+			this._queryValues.push(whereCaluse2)
+		}
+	} else {
+		this._queryValues.push(whereCaluse)
+		this._query += ` ${reservedWord} ? `
+	}
 
-  return this
+	return this
 }
