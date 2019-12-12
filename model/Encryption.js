eec34b4c0137756e0791188350db54904a8d399d
vivalalova <vivalalova0@gmail.com>
2019-12-12 11:56:06 +0800

    format

--- model/Encryption.js
+++ model/Encryption.js
@@ -6,20 +6,18 @@ const iv = Buffer.from(process.env.DATA_ENCRYPTION_IV, 'hex')
 
 module.exports = class Encryption {
 
-    static encrypt(text) {
-        const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
-        let encrypted = cipher.update(text)
-        encrypted = Buffer.concat([encrypted, cipher.final()])
-        return encrypted.toString('hex')
-    }
+	static encrypt(text) {
+		const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
+		let encrypted = cipher.update(text)
+		encrypted = Buffer.concat([encrypted, cipher.final()])
+		return encrypted.toString('hex')
+	}
 
-    static decrypt(text) {
-        const encryptedText = Buffer.from(text, 'hex')
-        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
-        let decrypted = decipher.update(encryptedText, 'binary')
-        decrypted = Buffer.concat([decrypted, decipher.final()])
-        return decrypted.toString()
-    }
-
-
+	static decrypt(text) {
+		const encryptedText = Buffer.from(text, 'hex')
+		const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
+		let decrypted = decipher.update(encryptedText, 'binary')
+		decrypted = Buffer.concat([decrypted, decipher.final()])
+		return decrypted.toString()
+	}
 }
