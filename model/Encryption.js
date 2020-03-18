d40a940be83736371aa18a6088cecdb8728cfa22
Hayden Huang <hayden@taxigo.com.tw>
2020-03-18 18:24:45 +0800

    add try catch to encyption

--- model/Encryption.js
+++ model/Encryption.js
@@ -10,17 +10,25 @@ const iv = Buffer.from(process.env.DATA_ENCRYPTION_IV, 'hex')
 module.exports = class Encryption {
 
 	static encrypt(text) {
-		const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
-		let encrypted = cipher.update(text)
-		encrypted = Buffer.concat([encrypted, cipher.final()])
-		return encrypted.toString('hex')
+		try {
+			const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
+			let encrypted = cipher.update(text)
+			encrypted = Buffer.concat([encrypted, cipher.final()])
+			return encrypted.toString('hex')
+		} catch(e) {
+			return ''
+		}
 	}
 
 	static decrypt(text) {
-		const encryptedText = Buffer.from(text, 'hex')
-		const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
-		let decrypted = decipher.update(encryptedText, 'binary')
-		decrypted = Buffer.concat([decrypted, decipher.final()])
-		return decrypted.toString()
+		try {
+			const encryptedText = Buffer.from(text, 'hex')
+			const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
+			let decrypted = decipher.update(encryptedText, 'binary')
+			decrypted = Buffer.concat([decrypted, decipher.final()])
+			return decrypted.toString()
+		} catch(e) {
+			return ''
+		}
 	}
 }
