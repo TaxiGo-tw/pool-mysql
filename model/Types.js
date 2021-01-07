bf85f74669a1d67f16f36d6677981315ecfe66f8
vivalalova <vivalalova0@gmail.com>
2021-01-07 17:02:07 +0800

    fix sql read query

--- model/Types.js
+++ model/Types.js
@@ -217,8 +217,10 @@ class SQLSelectOnlyString extends Str {
 				return false
 			}
 
-			const regex = /^SELECT (?!.*(?:CREATE |DROP |UPDATE |INSERT |ALTER |DELETE |ATTACH |DETACH |;)).*$/i
+			const regex = /^SELECT (?!.*(?:(\(| )CREATE |(\(| )DROP |(\(| )UPDATE |(\(| )INSERT |(\(| )ALTER |(\(| )DELETE |(\(| )ATTACH |(\(| )DETACH |;)).*$/i
+
 			return string
+				.trim()
 				.replace(/[ \n\t]+/g, ' ')
 				.match(regex)
 				.length > 0
