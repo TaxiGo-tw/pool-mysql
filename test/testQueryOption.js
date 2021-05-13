15ea49c933738ca02ae77c8eb8ec08c16d3079be
vivalalova <vivalalova0@gmail.com>
2021-05-13 16:35:09 +0800

    pass all

--- test/testQueryOption.js
+++ test/testQueryOption.js
@@ -30,9 +30,6 @@ describe('test Query Option', async () => {
 			.NESTED()
 			._options()
 
-
-
-
 		for (const key of Object.keys(options)) {
 			const value = options[key]
 			const type = typeof value
@@ -56,22 +53,36 @@ describe('test Query Option', async () => {
 						encryption: 'a'
 					}
 
-					console.log(key, ':', value)
 					value.should.equal(a[key])
 					break
 				}
-				case 'function':
-					console.log(type, key, ':', value + '')
+				case 'function': {
+					const a = {
+						mapCallback: d => d,
+						reduceCallback: (a, b) => a + b,
+						filter: d => d
+					}
+
+					const b = `${value}`
+					b.should.equal(`${a[key]}`)
 					break
-				case 'object':
-					console.log(type, key, ':', JSON.stringify(value))
+				}
+				case 'object': {
+					const a = {
+						decryption: ['abc'],
+						populates: [{ 'fk': { 'value': {} } }],
+						ex: { 'EX': 300, 'redisPrint': true },
+						query: { 'sql': 'SET @a := \'\';SELECT zzz_pool_mysql_testing.id, zzz_pool_mysql_testing.email SET ? FROM zzz_pool_mysql_testing WHERE (?) LIMIT ? AND ((SELECT @a := CONCAT_WS(\',\', IF(a IS NULL, "{{NULL}}",a), @a)) + 1);SELECT @a a', 'nestTables': true },
+						values: [{ 'a': 1 }, { 'id': 1 }, 1],
+					}
+
+					JSON.stringify(value).should.equal(JSON.stringify(a[key]))
 					break
+				}
 				default:
 					assert.fail('not handled', key, value)
 			}
 		}
-
-		// console.log(1, '\n', JSON.stringify(options))
 
 		const string = '{"query":{"sql":"SET @a := \'\';SELECT zzz_pool_mysql_testing.id, zzz_pool_mysql_testing.email SET ? FROM zzz_pool_mysql_testing WHERE (?) LIMIT ? AND ((SELECT @a := CONCAT_WS(\',\', IF(a IS NULL, \\"{{NULL}}\\",a), @a)) + 1);SELECT @a a","nestTables":true},"values":[{"a":1},{"id":1},1],"formatted":"SET @a := \'\';SELECT zzz_pool_mysql_testing.id, zzz_pool_mysql_testing.email SET `a` = 1 FROM zzz_pool_mysql_testing WHERE (`id` = 1) LIMIT 1 AND ((SELECT @a := CONCAT_WS(\',\', IF(a IS NULL, \\"{{NULL}}\\",a), @a)) + 1);SELECT @a a","reduceInitiVal":0,"nested":true,"print":true,"getFirst":true,"updated":true,"changedRows":1,"affectedRows":1,"onErr":"hi","decryption":["abc"],"populates":[{"fk":{"value":{}}}],"useWriter":true,"encryption":"a","ex":{"EX":300,"redisPrint":true}}'
 		JSON.stringify(options).should.equal(string)
