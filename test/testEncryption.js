ea777c2d252ecd1be6113b569f6ddd17f8413936
vivalalova <vivalalova0@gmail.com>
2021-05-13 14:24:01 +0800

    tested

--- test/testEncryption.js
+++ test/testEncryption.js
@@ -2,18 +2,9 @@ const { should, expect, assert } = require('chai')  // Using Assert style
 should()
 
 const pool = require('../index')
-
 const ZZZPoolMysqlTesting = require('./model/zzz_pool_mysql_testing')
 
 describe('test Encryption', async () => {
-
-	let connection
-
-	it('before', async () => {
-		connection = await pool.createConnection()
-		await connection.awaitTransaction()
-	})
-
 	it('should decrypt back', async () => {
 		const Encryption = require('../model/Encryption')
 		const string = 'abc'
@@ -25,18 +16,29 @@ describe('test Encryption', async () => {
 	})
 
 	it('should encrypt with Schema', async () => {
+		const connection = await pool.createConnection()
+		await connection.awaitTransaction()
+
 		const result = await ZZZPoolMysqlTesting
 			.INSERT()
 			.INTO()
-			.SET('email = ?', 'test@g.com', { encryption: ['email'] })
-			.PRINT()
-			.exec()
+			.SET({ email: 'test@g.com' }, undefined, { encryption: ['email'] })
+			.exec(connection)
 
 		result.affectedRows.should.equal(1)
-	})
 
-	it('after', () => {
-		connection.rollback()
+		const id = result.insertId
+
+		const [obj] = await ZZZPoolMysqlTesting.SELECT().FROM().WHERE({ id }).WRITER().exec(connection)
+		obj.id.should.equal(id)
+		obj.email.should.not.equal('test@g.com')
+
+		const [obj2] = await ZZZPoolMysqlTesting.SELECT().FROM().WHERE({ id }).DECRYPT('email').WRITER().exec(connection)
+		obj2.id.should.equal(id)
+		obj2.email.should.equal('test@g.com')
+
+
+		await connection.rollback()
 		connection.release()
 	})
 })
