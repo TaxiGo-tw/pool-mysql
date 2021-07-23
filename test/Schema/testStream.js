d17fd42b5f28d92f567c7df5f69ef6e49135ccf7
vivalalova <vivalalova0@gmail.com>
2021-07-23 19:55:00 +0800

    add test case

--- test/Schema/testStream.js
+++ test/Schema/testStream.js
@@ -5,6 +5,12 @@ const expect = require('chai').expect
 const assert = require('assert')
 
 describe('test stream', () => {
+	function toExpect(rows) {
+		expect(rows[0]).haveOwnProperty('trip_id')
+		expect(rows[0]).haveOwnProperty('user')
+	}
+
+
 	it('success', ok => {
 		Trips
 			.SELECT()
@@ -19,9 +25,8 @@ describe('test stream', () => {
 			.stream({
 				highWaterMark: 5,
 				onValue: (rows, done) => {
+					toExpect(rows)
 					assert.equal(rows.length, 5)
-					expect(rows[0]).haveOwnProperty('trip_id')
-					expect(rows[0]).haveOwnProperty('user')
 
 					done()
 				},
@@ -48,10 +53,34 @@ describe('test stream', () => {
 			.stream({
 				highWaterMark: 1,
 				onValue: (row, done) => {
-					expect(row).haveOwnProperty('trip_id')
-					expect(row).haveOwnProperty('user')
+					toExpect([row])
 
 					done()
+				},
+				onEnd: (error) => {
+					ok()
+				}
+			})
+	})
+
+	it('success async/await', ok => {
+		Trips
+			.SELECT()
+			.FROM()
+			.LEFTJOIN('user_info on user_info.uid = trips.user_id')
+			.LIMIT(30)
+			.NESTTABLES()
+			.MAP(data => {
+				const trip = data.trips
+				return {
+					...trip,
+					user: data.user_info
+				}
+			})
+			.stream({
+				highWaterMark: 1,
+				onValue: async (row, _) => {
+					toExpect([row])
 				},
 				onEnd: (error) => {
 					ok()
