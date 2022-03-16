33946904f8a846144cf104983567b31f074f0373
blueMike16 <mic8687@gmail.com>
2022-03-16 17:07:25 +0800

    fix testConnection add assertion

--- test/testConnection.js
+++ test/testConnection.js
@@ -174,7 +174,8 @@ describe('test connection reset _status.mustUpdateOneRow after mustUpdateOneRow 
 		const connection = pool.connection()
 
 		const sql = `UPDATE trips SET user_id = 101 WHERE user_id = 101 LIMIT 3;`
-		connection.mustUpdateOneRow.query(sql, () => {
+		connection.mustUpdateOneRow.query(sql, (err, result) => {
+			assert(err.message.includes('MUST_UPDATE_ONE_ROW'))
 			assert(connection._status.mustUpdateOneRow === false)
 			connection.release()
 			done()
