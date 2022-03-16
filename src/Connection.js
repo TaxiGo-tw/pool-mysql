d621e1615b39b9958fb30edd46dbc4b7c8d80d7a
blueMike16 <mic8687@gmail.com>
2022-03-16 16:33:01 +0800

    reset Connection._status.mustUpdateOneRow after mustUpdateOneRow error

--- src/Connection.js
+++ src/Connection.js
@@ -185,7 +185,7 @@ module.exports = class Connection {
 		Event.emit('did_query', this.identity(), query.sql)
 
 		if (mustUpdateOneRow && result && result.affectedRows != 1) {
-			this.resetStatus()
+			this._status.mustUpdateOneRow = false
 			throw Error(`MUST_UPDATE_ONE_ROW: ${query.sql}`)
 		}
 
